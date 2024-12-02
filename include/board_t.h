b8862f51f1c5a00db0622ad01b8a5bf8ecd490dd
Victor Martin <victormartin@MacBook-Pro-de-Victor-2.local>
2024-12-02 14:37:43 +0100

    implementation of board structure

--- include/board_t.h
+++ include/board_t.h
@@ -1,3 +1,12 @@
+struct piece_t {
+    char piece;    //¨ The piece type ('K', 'Q', 'R', 'B', 'N', 'P' for white, lowercase for black)
+    bool is_white; 
+};
+
+
 struct board_t {
-	// Add fields here
+	piece_t squares[8][8];
+    bool white_to_move;     // True if it's white's turn
+    int move_number;        // Current move number in the game
+    bool castling_rights[2];
 };
