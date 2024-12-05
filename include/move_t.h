6b117db263164c2e2719beeecf2e998d5a94a2b1
Leo Erdos <leo.erdos@polytechnique.edu>
2024-12-05 11:13:10 +0100

    Fix move_t header

--- include/move_t.h
+++ include/move_t.h
@@ -1,8 +1,9 @@
 #include <iostream>
 
 struct move_t {
-	int row;
-	int column;
+	std::pair<int, int> source;
+	std::pair<int, int> destination;
+
 	char promotion_code;
 
 	move_t(const std::string& move_spec);
