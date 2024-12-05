9759b5facdda64e2d2e2ab6d2c68663258e3d0f1
Leo Erdos <leo.erdos@polytechnique.edu>
2024-12-05 10:48:04 +0100

    Remove headers

--- include/evaluation.h
+++
@@ -1,8 +0,0 @@
-#include <board_t>
-
-namespace evaluation {
-	// This evaluation function returns the weighted sum of material
-	// If the position is winning for white, return a large positive value
-	// If the position is winning for black, return a large negative value
-	double by_material(board_t);
-};
--- include/moves.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include <board_t>
-
-struct move_t {
-	// Add fields here
-};
-
-std::vector<move_t> legal_moves(board_t);
