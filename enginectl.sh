16c14bb8796ccd8b642bd90806ed606dfa0534c0
Leo Erdos <leo.erdos@polytechnique.edu>
2024-12-07 23:51:47 +0100

    enginectl: Add test feature

--- enginectl.sh
+++ enginectl.sh
@@ -16,16 +16,42 @@ build() {
 	cd ..
 }
 
-while getopts ":b-:" option;
+_test() {
+	build
+	echo
+	for testname in tests/*
+	do
+		filename="TEST_$(basename -- "$testname")"
+		filename="${filename%.*}"
+		cp .cmake/$filename build/
+		echo -e -n "\033[1;4m$testname\033[0m: "
+		./build/$filename
+		if [ $? -eq 0 ]; then
+			echo -e "\033[32mTest passed.\033[0m"
+		else
+			echo -e "\033[31mTest failed.\033[0m"
+		fi
+		rm -f build/$filename
+	done
+}
+		
+
+while getopts ":bt-:" option;
 do
 	case $option in
 		b|build)
 			build
 			;;
+		t|test)
+			_test
+			;;
 		-)
 			case "${OPTARG}" in
 				build)
 					build
+					;;
+				test)
+					_test
 					;;
 			esac
 			;;
