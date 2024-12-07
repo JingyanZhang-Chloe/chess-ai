dc011e9e7ccde433e580402f3120a9ea80dee04a
张晶妍 <zhangjingyan@zhangjingyandeMacBook-Air.local>
2024-12-07 17:02:49 +0100

    Jingyan Add move_t.cppp and move_t.h

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 
 int main() {
 
+	move_t move = move_t("g1h1k");
+	std::cout << move.source.first << move.source.second << " " << move.destination.first << move.destination.second << " " << move.promotion_code.value_or(' ') << std::endl;
+
 	return 0;
 }
--- src/move_t.cpp
+++ src/move_t.cpp
@@ -3,29 +3,15 @@
 #include <helper.h>
 
 move_t::move_t(const std::string& move_spec){
-    std::cout << "here" << std::endl;
-    int i = 0;
-	for(char c : move_spec){
-        std::cout << "here2" << std::endl;
-        if(i == 0){
-            source.first = helper::chess_coord_to_int(c).value();
+    if(move_spec.size() == 4 || move_spec.size() == 5){
+        source.first = helper::chess_coord_to_int(move_spec[0]).value();
+        source.second = helper::chess_coord_to_int(move_spec[1]).value();
+        destination.first = helper::chess_coord_to_int(move_spec[2]).value();
+        destination.second = helper::chess_coord_to_int(move_spec[3]).value();
+        if(move_spec.size() == 5){
+            promotion_code = move_spec[4];
         }
-        else if(i == 1){
-            source.second = helper::chess_coord_to_int(c).value();
-        }
-        else if(i == 2){
-            destination.first = helper::chess_coord_to_int(c).value();
-        }        
-        else if(i == 3){
-            destination.second = helper::chess_coord_to_int(c).value();
-        }
-        else if(i == 4){
-            promotion_code = c;
-        }
-        else{
-            std::cout << "There's some error occur in the constructor of the move_t" << std::endl;
-        };
-
-        i += 1;
+    }else{
+        throw "There's some error occur in the constructor of the move_t";
     };
-}
+};
