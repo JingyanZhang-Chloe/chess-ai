a06c66f1a0c424c7027fbb67d4342aee4dedf9cb
Victor Martin <victormartin@MacBook-Pro-de-Victor-2.local>
2025-01-09 10:36:29 +0100

    update

--- exe/blunder-ai.cpp
+++ exe/blunder-ai.cpp
@@ -4,9 +4,9 @@
 #include <fstream>
 #include <move_t.h>
 
-int main() {
+int main(int argc, char ** argv) {
 	engine::board_t board;
-	std::ifstream inputFile("moves.txt");
+	std::ifstream inputFile(argv[2]);
 	for (std::string move_string; std::getline(inputFile, move_string);) {
 		engine::move_t move(move_string);
 		board.make_move(move);
@@ -14,7 +14,7 @@ int main() {
 
 	std::vector<engine::move_t> legal_moves = board.get_legal_moves();
 	//here we will use the function search in search_t.h instead of legal_moves[0]//
-	std::ofstream asciiFile("next_move.txt"); // dont know what path to put yet
+	std::ofstream asciiFile(argv[4]); // dont know what path to put yet
 	if(legal_moves.empty()){
 		std::cerr << "No possible legal moves" << std::endl;
 	}else {
