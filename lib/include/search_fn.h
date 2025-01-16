98846a56fd77a525436775bfc9c0ecd1a22af747
Victor Martin <victormartin@MacBook-Pro-de-Victor-2.local>
2025-01-16 15:42:50 +0100

    alpha beta final implementation with the class

--- lib/include/search_fn.h
+++ lib/include/search_fn.h
@@ -4,6 +4,15 @@
 
 namespace engine {
 
+namespace minmax_types {
+
+using cache_t = std::unordered_map<board_t::hash_t, std::pair<int, float>>;
+
+}
+
+float max_pass(board_t&, int depth, float alpha, float beta, minmax_types::cache_t&);
+float min_pass(board_t&, int depth, float alpha, float beta, minmax_types::cache_t&);
+
 float minmax(board_t&, int depth);
 
 }
