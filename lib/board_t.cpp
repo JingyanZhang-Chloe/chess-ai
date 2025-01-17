58744a49e4b43c08aad8eb11cd85c3bf3687407d
张晶妍 <zhangjingyan@zhangjingyandeMacBook-Air.local>
2025-01-17 18:00:30 +0100

    add is_ checkmate

--- lib/board_t.cpp
+++ lib/board_t.cpp
@@ -429,6 +429,26 @@ bool board_t::is_check() const {
 	return king_coords.has_value() && this->is_attacked(king_coords.value());
 }
 
+bool board_t::is_checkmate() {
+
+    if(!is_check()){
+        return false;
+    }
+
+    // Generate all possible moves for the current player
+    auto moves = pseudolegal_moves();
+
+    // Check if there is legal move
+    for(const auto& move : moves){
+        if(is_legal(move)){
+            return false; // Found a legal move, so not checkmate
+        }
+    }
+
+    // No legal moves and the player is in check
+    return true;
+}
+
 // TODO:  Optimization needed here, redundancy of legal_moves call.
 bool board_t::is_draw() {
 	// Case 1 : Stalemate
